5d4a68d5a9647a2914c0ea6001a0eb20909bac78
Azurene <98633770+Azurene@users.noreply.github.com>
2022-06-01 17:52:59 -0700

    snapshot 14.2

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const path = require('path');
 const express = require('express');
-const exphbs = require('express-handlebars');
 const session = require('express-session');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
@@ -19,8 +19,10 @@ const sess = {
   })
 };
 
+app.use(session(sess));
+
 const hbs = exphbs.create({});
- 
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -29,8 +31,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
-
-app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
